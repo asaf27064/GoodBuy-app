aff9492ecb1be8933e578c5a5522fb728a95643d
Asaf <asaf27064@gmail.com>
2025-08-05 20:56:23 +0300

    adding leave list

--- backend/src/routes/shoppingListRoutes.js
+++ backend/src/routes/shoppingListRoutes.js
@@ -8,5 +8,7 @@ router.get('/',    ctrl.getAllUserShoppingLists)
 router.post('/',   ctrl.createList)
 router.get('/:id', ctrl.getShoppingList)
 router.put('/:id', ctrl.updateListProducts)
+router.post('/:id/leave', ctrl.leaveList)
+
 
 module.exports = router
